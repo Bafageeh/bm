975cdeeb78cf6812924736d532216ab190704e25
Bafageeh <a.baf@live.com>
2026-05-17 14:49:43 +0300

    Add building model

---
+++ bm-api/app/Models/Building.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Building extends Model
+{
+    protected $fillable = [
+        'name',
+        'district',
+        'city',
+        'address',
+        'status',
+        'notes',
+    ];
+
+    public function managers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'building_managers')
+            ->withPivot(['role'])
+            ->withTimestamps();
+    }
+
+    public function apartments(): HasMany
+    {
+        return $this->hasMany(Apartment::class);
+    }
+
+    public function owners(): HasMany
+    {
+        return $this->hasMany(Owner::class);
+    }
+
+    public function expenses(): HasMany
+    {
+        return $this->hasMany(Expense::class);
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(OwnerPayment::class);
+    }
+}
