782713d19bbc8fcfd3696d85d9ab35f56596f96a
Bafageeh <a.baf@live.com>
2026-05-18 01:59:41 +0300

    Clear stale route cache before Laravel boot

--- bm-api/public/index.php
+++ bm-api/public/index.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+foreach (glob(__DIR__.'/../bootstrap/cache/routes*.php') ?: [] as $routesCacheFile) {
+    @unlink($routesCacheFile);
+}
+
 require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
