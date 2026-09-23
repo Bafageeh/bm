871702b2320b0a035dffcd9f24b4c4b26142c233
Bafageeh <a.baf@live.com>
2026-09-23 23:46:44 +0300

    Enforce owner edits through configured apartments only

--- bm-api/routes/api.php
+++ bm-api/routes/api.php
@@ -40,7 +40,6 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/owner/dashboard', [DashboardController::class, 'owner']);
 
     Route::get('/buildings/{building}/owners', [OwnerController::class, 'index']);
-    Route::put('/buildings/{building}/owners/{owner}', [OwnerController::class, 'update']);
     Route::put('/buildings/{building}/apartments/{apartment}/owner', [OwnerController::class, 'updateApartment']);
 
     Route::get('/buildings/{building}/expenses', [ExpenseController::class, 'index']);
